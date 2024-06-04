c9adbb91aa1fbf29174e4904fb4102ba9f0a5966
Hiren Patel <hiren@bytescout.com>
2024-06-04 18:47:25 +0530

    Initial Commit

---
+++ CustomerApi/Controllers/Customer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello There!");
+        }
+
+    }
+}
